85acf32bd9289c4f229c398745c0b2fd25008558
Kabrall <luizmiguelcabralf@outlook.com>
2026-01-24 19:29:56 -0300

    Primeiro commit de 2026

---
+++ Modulo_Um/01_Operacoes_Basicas.py
@@ -0,0 +1,11 @@
+import os
+
+arquivo1 = open('dados1.txt','w', encoding='utf-8')
+print(os.path.abspath(arquivo1.name))
+
+arquivo1.write('Olá, Mundo!')
+
+print(os.path.realpath(arquivo1.name))
+print(arquivo1)
+
+arquivo1.close()
